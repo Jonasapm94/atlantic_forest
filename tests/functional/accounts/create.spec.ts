d5fe4ae7ca0af6d9524486c00fa3a973b1f55e64
Jonas Ariel <jonas.ariel@codeminer42.com>
2025-11-11 17:45:59 -0300

    validation tests

--- tests/functional/accounts/create.spec.ts
+++ tests/functional/accounts/create.spec.ts
@@ -4,7 +4,7 @@ import hash from '@adonisjs/core/services/hash'
 import { faker } from '@faker-js/faker'
 import db from '@adonisjs/lucid/services/db'
 
-test.group('Accounts create', () => {
+test.group('Accounts create (model)', () => {
   test('hashes account password', async ({ assert }) => {
     const password = faker.internet.password()
     const email = faker.internet.email()
@@ -29,7 +29,7 @@ test.group('Accounts create', () => {
   })
 })
 
-test.group('Accounts create http request', () => {
+test.group('Accounts create (http request)', () => {
   test('returns status 201 with account properties, without password', async ({
     expect,
     client,
@@ -52,5 +52,106 @@ test.group('Accounts create http request', () => {
       id: expect.any(Number),
     })
     expect(response.body()).not.toHaveProperty('password')
+  })
+})
+
+test.group('invalid http body request', () => {
+  test('returns 422 with invalid name', async ({ expect, client }) => {
+    const name = faker.number.float()
+    const password = faker.internet.password()
+    const email = faker.internet.email()
+    const response = await client.post('/accounts').json({
+      name,
+      password,
+      email,
+    })
+
+    expect(response.status()).toBe(422)
+  })
+
+  test('returns 422 with missing name', async ({ expect, client }) => {
+    const name = null
+    const password = faker.internet.password()
+    const email = faker.internet.email()
+    const response = await client.post('/accounts').json({
+      name,
+      password,
+      email,
+    })
+
+    expect(response.status()).toBe(422)
+  })
+
+  test('returns 422 with invalid email', async ({ expect, client }) => {
+    const name = faker.person.fullName()
+    const password = faker.internet.password()
+    const email = faker.lorem.sentence()
+    const response = await client.post('/accounts').json({
+      name,
+      password,
+      email,
+    })
+
+    expect(response.status()).toBe(422)
+  })
+
+  test('returns 422 with missing email', async ({ expect, client }) => {
+    const name = faker.person.fullName()
+    const password = faker.internet.password()
+    const email = null
+    const response = await client.post('/accounts').json({
+      name,
+      password,
+      email,
+    })
+
+    expect(response.status()).toBe(422)
+  })
+
+  test('returns 422 with missing password', async ({ expect, client }) => {
+    const name = faker.person.fullName()
+    const password = null
+    const email = faker.internet.email()
+    const response = await client.post('/accounts').json({
+      name,
+      password,
+      email,
+    })
+
+    expect(response.status()).toBe(422)
+  })
+
+  test('returns 422 with missing email and password', async ({ expect, client }) => {
+    const name = faker.person.fullName()
+    const password = null
+    const email = null
+    const response = await client.post('/accounts').json({
+      name,
+      password,
+      email,
+    })
+
+    expect(response.status()).toBe(422)
+  })
+
+  test('returns 422 with missing name, email and password', async ({ expect, client }) => {
+    const name = null
+    const password = null
+    const email = null
+    const response = await client.post('/accounts').json({
+      name,
+      password,
+      email,
+    })
+
+    expect(response.status()).toBe(422)
+  })
+
+  test('returns 422 invalid params', async ({ expect, client }) => {
+    const response = await client.post('/accounts').json({
+      foo: 'bar',
+    })
+
+    expect(response.status()).toBe(422)
   })
 })
